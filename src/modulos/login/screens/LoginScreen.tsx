cce1272d141bb0c5c3e16b0ed129d3372dc40eb7
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-05-23 10:48:55 -0300

    feat: create module login

---
+++ src/modulos/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from '../styles/loginScreen.styles';
+
+const loginScreen = () => {
+  return (
+    <div>
+      <BackgroundImage src="./background.png" />
+    </div>
+  );
+};
+
+export default loginScreen;
