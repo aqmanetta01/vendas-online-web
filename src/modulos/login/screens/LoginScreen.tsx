b3598e9ce153899bdf380f5fa878e11ef1e0a1da
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-05-27 18:25:00 -0300

    feat: change layout and create button component

--- src/modulos/login/screens/LoginScreen.tsx
+++ src/modulos/login/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import Button from '../../../shared/buttons/button/Button';
 import Input from '../../../shared/inputs/input/input';
 import {
   BackgroundImage,
@@ -5,6 +6,7 @@ import {
   ContainerLoginScreen,
   LimitedContainer,
   LogoImage,
+  TitleLogin,
 } from '../styles/loginScreen.styles';
 
 const loginScreen = () => {
@@ -15,8 +17,14 @@ const loginScreen = () => {
       <ContainerLogin>
         <LimitedContainer>
           <LogoImage src="./logo.png" />
+          <TitleLogin level={2} type="secondary">
+            LOGIN
+          </TitleLogin>
           <Input title="Usuário" />
           <Input title="Senha" />
+          <Button type="primary" margin="64px 0px 16px 0px">
+            Entrar
+          </Button>
         </LimitedContainer>
       </ContainerLogin>
     </ContainerLoginScreen>
