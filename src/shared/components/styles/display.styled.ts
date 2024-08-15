2fcd2160c97d2efefd014c05ae2e760c4dfc5ea2
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-08-15 18:01:41 -0300

    feat: create table category

--- src/shared/components/styles/display.styled.ts
+++ src/shared/components/styles/display.styled.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
+interface DisplayFlexProps {
+  margin?: string;
+}
+
 export const DisplayFlex = styled.div`
   display: flex;
 `;
 
-export const DisplayFlexJustifySpaceBetween = styled(DisplayFlex)`
+export const DisplayFlexJustifyBetween = styled(DisplayFlex)<DisplayFlexProps>`
+  display: flex;
   justify-content: space-between;
+  ${(props) => (props.margin ? `margin: ${props.margin}` : '')};
 `;
