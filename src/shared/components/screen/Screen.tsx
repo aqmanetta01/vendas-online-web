be1f7c43e5bd35c76e6d65a347af19f0e6d4fc9e
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-08-15 13:34:01 -0300

    feat: insert menu in Screen

--- src/shared/components/screen/Screen.tsx
+++ src/shared/components/screen/Screen.tsx
@@ -1,18 +1,25 @@
+import { Divider } from 'antd';
+
 import Breadcrumb, { ListBreadcrumb } from '../breadcrumb/Breadcrumb';
+import Menu from '../menu/Menu';
 import { ScreenContainer } from './screen.style';
 
 interface ScreenProps {
   children: React.ReactNode;
   listBreadcrumb?: ListBreadcrumb[];
 }
-
 const Screen = ({ children, listBreadcrumb }: ScreenProps) => {
   return (
     <ScreenContainer>
-      {listBreadcrumb && <Breadcrumb listBreadcrumb={listBreadcrumb} />}
+      <Menu />
+      {listBreadcrumb && (
+        <>
+          <Breadcrumb listBreadcrumb={listBreadcrumb} />
+          <Divider />
+        </>
+      )}
       {children}
     </ScreenContainer>
   );
 };
-
 export default Screen;
--- src/shared/components/screen/screen.style.ts
+++ src/shared/components/screen/screen.style.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const ScreenContainer = styled.div`
+  width: calc(100% - 368px);
   padding: 32px;
+  margin: 32px;
+  background-color: white;
+  margin-left: auto;
 `;
