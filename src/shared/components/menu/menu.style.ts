5e652b61091ce1e6715c5efa8e61c443f348b5ff
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-08-15 16:24:19 -0300

    feat: insert navigate menu

--- src/shared/components/menu/menu.style.ts
+++ src/shared/components/menu/menu.style.ts
@@ -11,8 +11,8 @@ export const ContainerMenu = styled.div`
   top: 0;
   bottom: 0;
   background-color: #14253d;
+  background-color: #001529;
   width: 240px;
-
   -webkit-box-shadow: 1px 0px 8px 0px rgba(0, 0, 0, 0.71);
   -moz-box-shadow: 1px 0px 8px 0px rgba(0, 0, 0, 0.71);
   box-shadow: 1px 0px 8px 0px rgba(0, 0, 0, 0.71);
@@ -23,7 +23,6 @@ export const ContainerLogoName = styled.div`
   height: 72px;
   display: flex;
   align-items: center;
-
   -webkit-box-shadow: -2px 6px 4px 0px rgba(0, 0, 0, 0.47);
   -moz-box-shadow: -2px 6px 4px 0px rgba(0, 0, 0, 0.47);
   box-shadow: -2px 6px 4px 0px rgba(0, 0, 0, 0.47);
