ddecba70d2a0d5db8d8efe301ce7008b46346eea
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-08-20 15:50:32 -0300

    feat: finish category screen

--- src/modulos/category/hooks/useInsertCategory.ts
+++ src/modulos/category/hooks/useInsertCategory.ts
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { URL_CATEGORY } from '../../../shared/constants/urls';
+import { MethodsEnum } from '../../../shared/enums/methods.enums';
+import { useDataContext } from '../../../shared/hooks/useDataContext';
+import { useRequests } from '../../../shared/hooks/useRequests';
+import { CategoryRoutesEnum } from '../routes';
 
 export const useInsertCategory = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [loading, setLoading] = useState(false);
+  const [disabledButton, setDisabledButton] = useState(true);
 
-  const insertCategory = () => {
+  const { setCategories } = useDataContext();
+
+  useEffect(() => {
+    if (!name) {
+      setDisabledButton(true);
+    } else {
+      setDisabledButton(false);
+    }
+  }, [name]);
+
+  const insertCategory = async () => {
     setLoading(true);
+    const { request } = useRequests();
+    await request(URL_CATEGORY, MethodsEnum.POST, undefined, { name });
+    await request(URL_CATEGORY, MethodsEnum.GET, setCategories);
+    setLoading(false);
+    navigate(CategoryRoutesEnum.CATEGORY);
   };
 
   const handleOnChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -13,6 +37,7 @@ export const useInsertCategory = () => {
   };
 
   return {
+    disabledButton,
     name,
     handleOnChangeName,
     insertCategory,
