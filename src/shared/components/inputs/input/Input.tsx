d13aab43d3600bfd11418923740608f36e304fbc
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-08-22 17:29:24 -0300

    feat: insert unit test in ProductInsert

--- src/shared/components/inputs/input/Input.tsx
+++ src/shared/components/inputs/input/Input.tsx
@@ -1,7 +1,7 @@
 import { Input as InputAntd, InputProps as InputPropsAntd } from 'antd';
 
-import { BoxInput, TitleInput } from './input.styles';
 import { InputTestIdEnum } from './enum/inputTestIdEnum';
+import { BoxInput, TitleInput } from './input.styles';
 
 export interface InputProps extends InputPropsAntd {
   title?: string;
